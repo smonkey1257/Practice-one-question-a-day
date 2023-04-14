018e02a479e0aefb1bdb153dfb0a27e8ad7da0b2
smonkey1257 <15219574073@139.com>
2023-04-14 19:53:15 +0800

    堆栈操作合法性

--- Practice_4_14/test.c
+++ Practice_4_14/test.c
@@ -72,3 +72,39 @@ int main(void)
     }
     return 0;
 }
+
+//写法二：
+//出处：
+https://blog.csdn.net/weixin_43838785/article/details/102672346?ydreferer=aHR0cHM6Ly9jbi5iaW5nLmNvbS8%3D
+#include<stdio.h>
+
+int main()
+{
+    //输入栈容量和操作数，用length表示栈内现有元素个数
+    int Ssize, oper, counter, length = 0, flag = 0;
+    scanf("%d %d ", &oper, &Ssize);
+    for (counter = 0; counter < oper; counter++) {
+        char chr;
+        //s入栈，length加一,x出栈，length减一
+        while ((chr = getchar()) != '\n') {
+            if (chr == 'S') {
+                length++;
+                if (length > Ssize)
+                    flag = 1;
+            }
+            if (chr == 'X') {
+                length--;
+                if (length < 0)
+                    flag = 1;
+            }
+        }
+        if (length == 0 && flag == 0)
+            printf("YES\n");
+        else
+            printf("NO\n");
+        length = 0;
+        flag = 0;
+    }
+    return 0;
+}
+
